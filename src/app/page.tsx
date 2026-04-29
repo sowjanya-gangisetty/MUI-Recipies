af1ea19a295c925fc0d734027802ff7cc4119f3a
sowjanya-gangisetty <sowji.gsetty@gmail.com>
2026-04-29 11:24:53 +0200

    GUI Container BOX

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
 
 import styles from "./page.module.css";
-import { Typography , Button} from "@mui/material";
+import { Typography , Button, Container, Paper, Box} from "@mui/material";
 import RestaurantMenuIcon from '@mui/icons-material/RestaurantMenu';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <Container maxWidth="md">
+      <Paper elevation={2}>
+        <Box sx={{
+          m: 6,
+          p:4,
+          textAlign:"center",
+        }}>
         <Typography variant="h1" align="center" color="textPrimary"> Welcome to My recipies</Typography>
-        <Button href="/recipe"  startIcon={<RestaurantMenuIcon/ >} size="large" variant="outlined" color="secondary">Click me!!</Button>
-    </div>
+        <Button href="/recipe"  startIcon={<RestaurantMenuIcon/ >} 
+                size="large" variant="outlined" color="secondary"
+                sx={{
+                  m:4,
+                  borderRadius:"50px"
+                }}>
+            Click me!
+        </Button>
+      </Box>
+      </Paper>
+    </Container>
   );
 }
--- src/app/recipe/page.tsx
+++ src/app/recipe/page.tsx
@@ -5,7 +5,7 @@ const RecipePage = () =>{
     return(
         <div>
             <Typography variant="h2" align="center" color="textPrimary">This is the Recipe page</Typography>
-            <Button ></Button>
+            <Button href="/">Back</Button>
         </div>
     )
 }
