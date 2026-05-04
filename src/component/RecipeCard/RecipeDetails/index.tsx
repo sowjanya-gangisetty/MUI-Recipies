37a08de3242d348186dc796b585663a1422bcae5
sowjanya-gangisetty <sowji.gsetty@gmail.com>
2026-05-04 10:41:56 +0200

    tabs

---
+++ src/component/RecipeCard/RecipeDetails/index.tsx
@@ -0,0 +1,77 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import Tabs from '@mui/material/Tabs';
+import { Typography } from '@mui/material';
+import CardMedia from '@mui/material/CardMedia';
+
+type RecipeDeatisType = {
+    instructions: string,
+    video: string,
+    ingredients:string[]
+}
+
+export default function RecipeDetails({ instructions, video, ingredients }: RecipeDeatisType) {
+    const [value, setValue] = React.useState('1');
+
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+        setValue(newValue);
+    };
+
+    const embedUrl = video.replace("watch?v=", "embed/")
+
+    return (
+        <Box sx={{ width: '100%', typography: 'body1' }}>
+            <TabContext value={value}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <TabList onChange={handleChange} aria-label="lab API tabs example">
+                        <Tab label="Instructions" value="1" />
+                        <Tab label="Ingredients" value="2" />
+                        <Tab label="Video" value="3" />
+                    </TabList>
+                </Box>
+                <TabPanel value="1">
+                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                        Instructions : {instructions}
+                    </Typography>
+                </TabPanel>
+                <TabPanel value="2">
+                    <Box
+                        component="ul"
+                        sx={{
+                            listStyleType: "circle",
+                            display: "flex",
+                            flexDirection: "column",
+                            margin: 0,
+                            fontSize: 22
+                        }}
+                    >
+                        {ingredients.map((item, index) => (
+                            <Box
+                                component="li"
+                                key={index}
+                                sx={{
+                                    textAlign: "left",
+                                }}
+                            >
+                                <Typography variant="body1">{item}</Typography>
+                            </Box>
+                        ))}
+                    </Box>
+                </TabPanel>
+                <TabPanel value="3">Item Three
+                    {embedUrl &&
+                        <CardMedia
+                            component="iframe"
+                            height="194"
+                            src={embedUrl}
+                        />
+                    }
+                </TabPanel>
+            </TabContext>
+        </Box>
+    );
+}
--- src/component/RecipeCard/index.tsx
+++ src/component/RecipeCard/index.tsx
@@ -14,11 +14,12 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import type { RecipeType } from '@/app/types/types';
 import { Box } from '@mui/material';
+import RecipeDetails from './RecipeDetails';
 
 
 export default function RecipeCard({ name, area, category, instructions, image, video, ingredients }: RecipeType) {
 
-    const embedUrl = video.replace("watch?v=", "embed/")
+    //const embedUrl = video.replace("watch?v=", "embed/")
 
     return (
         <Card sx={{ maxWidth: "md" }}>
@@ -37,44 +38,8 @@ export default function RecipeCard({ name, area, category, instructions, image, 
                 alt={name}
                 src={image}
             />
-            <CardContent>
-                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                    Instructions : {instructions}
-                </Typography>
-                <Typography variant="h6" sx={{ paddingBottom:1 }}>
-                    Ingredients
-                </Typography>
-
-                <Box
-                    component="ul"
-                    sx={{
-                        listStyleType: "circle",
-                        display: "flex",
-                        flexDirection: "column",
-                        margin:0 ,
-                        fontSize:22
-                    }}
-                > 
-                    {ingredients.map((item, index) => (
-                        <Box
-                            component="li"
-                            key={index}
-                            sx={{
-                                textAlign: "left",
-                            }}
-                        >
-                            <Typography variant="body1">{item}</Typography>
-                        </Box>
-                    ))}
-                </Box>
-            </CardContent>
-            {embedUrl &&
-                <CardMedia
-                    component="iframe"
-                    height="194"
-                    src={embedUrl}
-                />
-            }
+            <RecipeDetails instructions={instructions} video={video} ingredients={ingredients}/>
+       
             <CardActions disableSpacing>
                 <IconButton aria-label="add to favorites">
                     <FavoriteIcon />
